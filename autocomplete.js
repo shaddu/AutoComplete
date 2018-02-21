cfe20593eff34a60b1a472f8cf5f4e3cb017f7c9
shaddu <shadab.ahmed63@gmail.com>
2018-02-21 01:35:32 +0000

    onfocus event added

--- autocomplete.js
+++ autocomplete.js
@@ -1,43 +1,41 @@
-(function() {
-document.getElementById("search").onkeyup = debounce(inputSpy, 100, true);
-
-function inputSpy(event) {
-    askForSuggestions(event.target.value, outputResults);
-}
-
-function outputResults(response_text) {
-    var result_display = document.getElementById("result");
-    result_display.innerHTML = response_text;
-}
-
-function askForSuggestions(input_text, result_processor) {
+function Auto(id, timeout, immediate) {
+    document.getElementById(id).onkeyup = debounce(inputSpy, timeout, immediate);
+    document.getElementById(id).onfocus = debounce(inputSpy, timeout, immediate);
+    
     var trie = createTrie();
-    _data.forEach(function(e){
+    _data.forEach(function (e) {
         console.log(e.name)
         trie.insert(e.name);
     });
 
-    var list = trie.autoComplete(input_text);
-    //list.forEach(function(e))
-   
-    result_processor(list);
+    function inputSpy(event) {
+        askForSuggestions(event.target.value, outputResults);
+    }
 
-}
+    function outputResults(response_text) {
+        var result_display = document.getElementById("result");
+        result_display.innerHTML = response_text;
+    }
 
-function debounce(func, wait, immediate) {
-    var timeout, result;
-    return function () {
-        var context = this,
-            args = arguments;
-        var later = function () {
-            timeout = null;
-            if (!immediate) result = func.apply(context, args);
+    function askForSuggestions(input_text, result_processor) {
+        var list = trie.autoComplete(input_text);
+        result_processor(list);
+    }
+
+    function debounce(func, wait, immediate) {
+        var timeout, result;
+        return function () {
+            var context = this,
+                args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) result = func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) result = func.apply(context, args);
+            return result;
         };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) result = func.apply(context, args);
-        return result;
-    };
-}
-})();
+    }
+};
