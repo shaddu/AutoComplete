50f329044eecf14c8c9ea1b3fe87016cc6d54115
shaddu <shadab.ahmed63@gmail.com>
2018-02-21 02:17:39 +0000

    little ui

--- autocomplete.js
+++ autocomplete.js
@@ -1,7 +1,13 @@
 function Auto(id, timeout, immediate) {
     document.getElementById(id).onkeyup = debounce(inputSpy, timeout, immediate);
     document.getElementById(id).onfocus = debounce(inputSpy, timeout, immediate);
-    
+    document.getElementById(id).onblur = clear();
+
+
+    function clear() {
+        document.getElementById(id + "-suggestions").innerHTML = "";
+    }
+
     var trie = createTrie();
     _data.forEach(function (e) {
         console.log(e.name)
@@ -13,8 +19,13 @@ function Auto(id, timeout, immediate) {
     }
 
     function outputResults(response_text) {
-        var result_display = document.getElementById("result");
-        result_display.innerHTML = response_text;
+        var response_html = "<ol>";
+        var result_display = document.getElementById(id + "-suggestions");
+        response_text.forEach(function (e, i) {
+            response_html += "<li><span>" + (i + 1) + "</span><p>" + e + "</p></li>";
+        })
+        response_html += "</ol>";
+        result_display.innerHTML = response_html;
     }
 
     function askForSuggestions(input_text, result_processor) {
